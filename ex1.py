1183f5c45613c793a86d3455f9e85df9b3ed64cd
Quentin <brilhacquentin@gmail.com>
2026-09-11 15:19:40 +0200

    tp1_DLC

---
+++ ex1.py
@@ -0,0 +1,16 @@
+print("Quel exercice voulez vous voir ? \n -1 : IMC \n -2 : maxminmoy\n -3 : age canin\n -4 : approximation de pi\n -5 : traduction en binaire\n -6 : calculatrice college\n -7 : createur de plaque")
+choix=int(input("choisissez votre programme : "))
+if choix==1:
+    import exo1
+elif choix==2:
+    import exo2
+elif choix==3:
+    import exo3
+elif choix==4:
+    import exo4
+elif choix==5:
+    import exo5
+elif choix==6:
+    import exo6
+elif choix==7:
+    import exo7
