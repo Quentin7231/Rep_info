7a8a240503424e9dbb6e0131bc3e845dda734348
FatihhanCog <fatihhan.cog@reseau.eseo.fr>
2026-09-24 08:29:46 +0200

    exo6 tp2

---
+++ TP2/exo6.py
@@ -0,0 +1,17 @@
+import requests 
+
+def get_university_data(country = "France"):
+    url = f"http://universities.hipolabs.com/search?country={country}"
+
+    rawdata = requests.get(url)
+
+    if not rawdata:
+        raise Exception
+
+    data = rawdata.json()
+    return data
+
+
+if __name__ == "__main__":
+    uni_data = get_university_data("France")
+    print(uni_data)     
